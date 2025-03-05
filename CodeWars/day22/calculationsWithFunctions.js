5bd0f713dd0491a2c590e8c89c4a90c9fabb5a0c
Orest Hlavatskyi <_>
2025-03-05 22:22:02 +0200

    Every day kata commit

---
+++ CodeWars/day22/calculationsWithFunctions.js
@@ -0,0 +1,68 @@
+// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+// Requirements:
+
+// There must be a function for each number from 0 ("zero") to 9 ("nine")
+// There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
+// Each calculation consist of exactly one operation and two numbers
+// The most outer function represents the left operand, the most inner function represents the right operand
+// Division should be integer division. For example, this should return 2, not 2.666666...:
+// eight(dividedBy(three()));
+
+function zero(fn) {
+  return fn ? fn(0) : 0;
+}
+function one(fn) {
+  return fn ? fn(1) : 1;
+}
+function two(fn) {
+  return fn ? fn(2) : 2;
+}
+function three(fn) {
+  return fn ? fn(3) : 3;
+}
+function four(fn) {
+  return fn ? fn(4) : 4;
+}
+function five(fn) {
+  return fn ? fn(5) : 5;
+}
+function six(fn) {
+  return fn ? fn(6) : 6;
+}
+function seven(fn) {
+  return fn ? fn(7) : 7;
+}
+function eight(fn) {
+  return fn ? fn(8) : 8;
+}
+function nine(fn) {
+  return fn ? fn(9) : 9;
+}
+
+function plus(secondArgument) {
+  return function (firstArgument) {
+    return firstArgument + secondArgument;
+  };
+}
+function minus(secondArgument) {
+  return function (firstArgument) {
+    return firstArgument - secondArgument;
+  };
+}
+function times(secondArgument) {
+  return function (firstArgument) {
+    return firstArgument * secondArgument;
+  };
+}
+function dividedBy(secondArgument) {
+  return function (firstArgument) {
+    return Math.floor(firstArgument / secondArgument);
+  };
+}
+
+console.log(eight(plus(three())));
